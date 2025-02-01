79017fd0afe68be2d822e5ea1e854403622751b0
Tuan Wannafly <todangminhtuan123@gmail.com>
2025-02-02 02:13:55 +0700

    12 (Part 2) Advanced Functions and optimized code more leaner

--- 1 variables/scripts/12_rock_paper_scissor.js
+++ 1 variables/scripts/12_rock_paper_scissor.js
@@ -21,7 +21,7 @@ let intervalID;
 
 function autoPlay() {
     if(!isAutoPlaying) {
-        intervalID = setInterval(function() {
+        intervalID = setInterval(() => {
             const playerMove = pickComputerMove();
             playGame(playerMove);
         }, 1000);
@@ -33,6 +33,26 @@ function autoPlay() {
 }
 
     //localStorage.getItem('score');
+    document.querySelector('.js-rock-button').addEventListener('click', () => {
+        playGame('rock');
+    });
+    document.querySelector('.js-paper-button').addEventListener('click', () => {
+        playGame('paper');
+    });
+    document.querySelector('.js-scissors-button').addEventListener('click', () => {
+        playGame('scissors');
+    });
+
+    document.body.addEventListener('keydown', (event) => {
+        if(event.key === 'r') {
+            playGame('rock');
+        } else if(event.key === 'p') {
+            playGame('paper');
+        } else if(event.key === 's') {
+            playGame('scissors');
+        }
+    });
+
 
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
--- 1 variables/scripts/12_todo_list.js
+++ 1 variables/scripts/12_todo_list.js
@@ -9,26 +9,41 @@ const todoList = [{
 
 renderTodoList();
 
+//const renderTodoList = () => {
+    // another way to call function
+//};
+
 function renderTodoList() {
 
     let todoListHTML = '';
 
-    todoList.forEach(function(todoObject, index) {
+    todoList.forEach((todoObject, index)  => {
         const {name, dueDate} = todoObject;
         const html = `
             <div>${name}</div>
             <div>${dueDate}</div> 
-            <button onclick = "
-                todoList.splice(${index}, 1);
-                renderTodoList();
-            " class = "delete-todo-button">Delete</button>
+            <button class = "delete-todo-button js-delete-todo-button">Delete</button>
         `; // Generating the HTML
         todoListHTML += html;
 
     }); 
 
-    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+    document.querySelectorAll('.js-delete-todo-button')
+        .forEach((deleteButton, index) => {
+            deleteButton.addEventListener('click', () => {
+                todoList.splice(index, 1);
+                renderTodoList();                
+            });
+
+        });
 }
+
+document.querySelector('.js-add-todo-button')
+    .addEventListener('click', () => {
+        addTodo();
+    });
 
 
 
