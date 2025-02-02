49609d5ee08de76d5edc7e8ec97fb611d6a233b4
Tuan Wannafly <todangminhtuan123@gmail.com>
2025-02-02 07:31:21 +0700

    make the quantity cart interactive

--- 1 variables/javascript-amazon-project-main/scripts/amazon.js
+++ 1 variables/javascript-amazon-project-main/scripts/amazon.js
@@ -126,6 +126,14 @@ document.querySelectorAll('.js-add-to-cart')
 
             }
 
+            let cartQuantity = 0;
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+            console.log(cartQuantity);
 
             console.log(cart);
         });
