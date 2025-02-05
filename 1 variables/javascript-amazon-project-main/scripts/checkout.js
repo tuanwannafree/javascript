70bbb3320f39c69175124c9d7d2c944f86bf99cb
Tuan Wannafly <todangminhtuan123@gmail.com>
2025-02-05 22:06:52 +0700

    Delete products from HTML when clicking delete

--- 1 variables/javascript-amazon-project-main/scripts/checkout.js
+++ 1 variables/javascript-amazon-project-main/scripts/checkout.js
@@ -18,7 +18,8 @@ cart.forEach((cartItem) => {
     //console.log(matchingProduct);
 
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container 
+        js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
                 </div>
@@ -111,7 +112,9 @@ document.querySelectorAll('.js-delete-link')
 
             removeFromCart(productId);
 
-            console.log(cart);
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
+            //console.log(cart);
 
         });
     }); 
