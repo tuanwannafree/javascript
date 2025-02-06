1bf32e7a1c1fb91a5b91a42c8b217a85114f3718
Tuan Wannafly <todangminhtuan123@gmail.com>
2025-02-06 22:29:34 +0700

    another bug

--- 1 variables/javascript-amazon-project-main/scripts/checkout.js
+++ 1 variables/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import  formatCurrency  from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -103,7 +103,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
         html += 
         `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option
+                data-product-id="${matchingProduct.id}"
+                data-delivery-option-id="${deliveryOption.id}">
                 <input type="radio"
                         ${isChecked ? 'check' : ''}
                         class="delivery-option-input"
@@ -142,3 +144,11 @@ document.querySelectorAll('.js-delete-link')
 
         });
     }); 
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+        element.addEventListener('click', () => {
+            const {productId, deliveryOptionId} = element.dataset; // shorthand Property cosnt productId = element.dataset.productId;
+            updateDeliveryOption(productId, deliveryOptionId);      // const deliveryOptionId = element.dataset.deliveryOptionId;
+        });
+    });
