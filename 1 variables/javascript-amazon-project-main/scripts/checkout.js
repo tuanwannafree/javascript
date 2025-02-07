5fa7540717931e8eb20847068dd65c7da866e042
Tuan Wannafly <todangminhtuan123@gmail.com>
2025-02-08 03:53:48 +0700

    make delivery option interactive - uncompleted

--- 1 variables/javascript-amazon-project-main/scripts/checkout.js
+++ 1 variables/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -46,46 +46,45 @@ cart.forEach((cartItem) => {
     //console.log(matchingProduct);
 
     cartSummaryHTML += `
-        <div class="cart-item-container 
-        js-cart-item-container-${matchingProduct.id}">
-            <div class="delivery-date">
-                Delivery date: ${dateString}
-                </div>
+    <div class="cart-item-container
+      js-cart-item-container-${matchingProduct.id}">
+      <div class="delivery-date">
+        Delivery date: ${dateString}
+      </div>
 
-                <div class="cart-item-details-grid">
-                <img class="product-image"
-                    src="${matchingProduct.image}">
+      <div class="cart-item-details-grid">
+        <img class="product-image"
+          src="${matchingProduct.image}">
 
-                <div class="cart-item-details">
-                    <div class="product-name">
-                        ${matchingProduct.name}
-                    </div>
-                    <div class="product-price">
-                        ${formatCurrency(matchingProduct.priceCents)}
-                    </div>
-                    <div class="product-quantity">
-                    <span>
-                        Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-                    </span>
-                    <span class="update-quantity-link link-primary">
-                        Update
-                    </span>
-                    <span class="delete-quantity-link link-primary 
-                    js-delete-link" data-product-id="${matchingProduct.id}">
-                        Delete
-                    </span>
-                    </div>
-                </div>
-
-                <div class="delivery-options">
-                    <div class="delivery-options-title">
-                    Choose a delivery option:
-                    </div>
-                    ${deliveryOptionsHTML(matchingProduct, cartItem)}
-                </div>
-            </div>
+        <div class="cart-item-details">
+          <div class="product-name">
+            ${matchingProduct.name}
+          </div>
+          <div class="product-price">
+            $${formatCurrency(matchingProduct.priceCents)}
+          </div>
+          <div class="product-quantity">
+            <span>
+              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+            </span>
+            <span class="update-quantity-link link-primary">
+              Update
+            </span>
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              Delete
+            </span>
+          </div>
         </div>
-    `
+
+        <div class="delivery-options">
+          <div class="delivery-options-title">
+            Choose a delivery option:
+          </div>
+          ${deliveryOptionsHTML(matchingProduct, cartItem)}
+        </div>
+      </div>
+    </div>
+  `;
 });
 
 
@@ -108,21 +107,23 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
         html += `
-            <div class="delivery-option">
-                    <input type="radio"
-                    ${isChecked ? 'checked' : ''}
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                    <div>
-                    <div class="delivery-option-date">
-                        ${dateString}
-                    </div>
-                    <div class="delivery-option-price">
-                        ${priceString}Shipping
-                    </div>
-                </div>
+        <div class="delivery-option js-delivery-option"
+          data-product-id="${matchingProduct.id}"
+          data-delivery-option-id="${deliveryOption.id}">
+          <input type="radio"
+            ${isChecked ? 'checked' : ''}
+            class="delivery-option-input"
+            name="delivery-option-${matchingProduct.id}">
+          <div>
+            <div class="delivery-option-date">
+              ${dateString}
             </div>
-        `
+            <div class="delivery-option-price">
+              ${priceString} Shipping
+            </div>
+          </div>
+        </div>
+      `
     });
 
     return html;
@@ -149,3 +150,13 @@ document.querySelectorAll('.js-delete-link')
 
         });
     }); 
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+        element.addEventListener('click', () => {
+            const { productId, deliveryOptionId } = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId);
+            console.log('Updated cart:', cart);
+
+        });
+    });
